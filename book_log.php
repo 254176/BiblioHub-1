8799680db675e551474823e1c124100712581c03
Jaidyn Ann <10477760+JadedCtrl@users.noreply.github.com>
2024-04-16 17:04:33 -0500

    More informative book-log row-presentation
    Display Books.Title, Books.ISBN, Users.Name,
    Users.UserID, Transactions.TransactionDate,
    Transactons.DueDate, and Transaction.State;
    rather than just columns from the Transactions
    table.

--- book_log.php
+++ book_log.php
@@ -27,12 +27,13 @@ die( $e->getMessage() );
 		<table>
 			<thead>
 				<tr>
-					<th>Transaction ID</th>
+					<th>Title</th>
+					<th>ISBN</th>
+					<th>Full name</th>
 					<th>User ID</th>
-					<th>Inventory ID</th>
-					<th>Transaction Type</th>
-					<th>Transaction Date</th>
+					<th>Date</th>
 					<th>Due Date</th>
+					<th>Type</th>
 					<th>Status</th>
 				</tr>
 			</thead>
@@ -40,19 +41,28 @@ die( $e->getMessage() );
 			<tbody>
 				<?php
 				// Fetch book log data from the database and populate the table
-				$statement = 'select * from Transactions';
+				$statement = <<<END
+								SELECT u.Name, u.UserID, b.Title, b.ISBN, t.TransactionType, t.TransactionDate, t.DueDate, t.State
+								FROM Books as b, Inventory as i, Transactions as t, Users as u
+								WHERE b.ISBN = i.ISBN AND t.InventoryID = i.InventoryID
+								AND u.UserID = t.UserID
+								END;
+								// >>>
+
+				$transaction_types = ["Checked-out", "Returned", "Added", "Removed"];
 				$result = $db->query($statement);
 				while ($row = $result->fetch())
 				{
-					echo("<tr>");
-					echo("<td>" . $row['TransactionID'] . "</td>");
-					echo("<td>" . $row['UserID'] . "</td>");
-					echo("<td>" . $row['InventoryID'] . "</td>");
-					echo("<td>" . $row['TransactionType'] . "</td>");
-					echo("<td>" . $row['TransactionDate'] . "</td>");
-					echo("<td>" . $row['DueDate'] . "</td>");
-					echo("<td>" . $row['State'] . "</td>");
-					echo("</tr>");
+				echo("<tr>");
+				echo("<td>" . $row['Title'] . "</td>");
+				echo("<td>" . $row['ISBN'] . "</td>");
+				echo("<td>" . $row['Name'] . "</td>");
+				echo("<td>" . $row['UserID'] . "</td>");
+				echo("<td>" . $row['TransactionDate'] . "</td>");
+				echo("<td>" . $row['DueDate'] . "</td>");
+				echo("<td>" . $transaction_types[$row['TransactionType']] . "</td>");
+				echo("<td>" . ($row['State'] ? "✓" : "⦵") . "</td>");
+				echo("</tr>");
 				}
 				?>
 			</tbody>
