046251c0205b1e6afee9807f5078fcc421c2e188
Jaidyn Ann <10477760+JadedCtrl@users.noreply.github.com>
2024-04-16 17:08:49 -0500

    Add additional testing data to load.sql
    More check-outs, returns, etc.

--- sql/load.sql
+++ sql/load.sql
@@ -26,10 +26,22 @@ INSERT INTO Users
 INSERT INTO Transactions
        (TransactionID, UserID, InventoryID, TransactionType, TransactionDate, DueDate, State)
        VALUES
-       (1001, 1, 1, 0, '2024-04-01 09:00:00', '2024-04-08 09:00:00', 1),
-       (1002, 2, 2, 0, '2024-04-02 10:00:00', '2024-04-09 10:00:00', 1),
-       (1003, 3, 3, 0, '2024-04-03 11:00:00', '2024-04-10 11:00:00', 1),
-       (1004, 1, 4, 0, '2024-04-04 12:00:00', '2024-04-11 12:00:00', 1);
+       -- The librarian (Charlie) registers four books…
+       (1001, 3, 1, 2, '2024-04-01 09:00:00', NULL, true),
+       (1002, 3, 2, 2, '2024-04-01 10:00:00', NULL, true),
+       (1003, 3, 3, 2, '2024-04-01 11:00:00', NULL, true),
+       (1004, 3, 4, 2, '2024-04-01 12:00:00', NULL, true),
+
+       -- … which are checked out three days later.
+       (1005, 1, 1, 0, '2024-04-04 09:00:00', '2024-04-11 09:00:00', false),
+       (1006, 2, 2, 0, '2024-04-05 10:00:00', '2024-04-12 10:00:00', false),
+       (1007, 3, 3, 0, '2024-04-06 11:00:00', '2024-04-13 11:00:00', false),
+       (1008, 1, 4, 0, '2024-04-07 12:00:00', '2024-04-14 12:00:00', false),
+
+       -- Two are returned on time, one is returned late, one is never returned.
+       (1009, 1, 1, 1, '2024-04-07 11:00:00', NULL, true),
+       (1010, 1, 4, 1, '2024-04-07 11:03:00', NULL, true),
+       (1011, 2, 2, 1, '2024-04-13 11:03:00', NULL, true);
 
 -- Insert data into the Events table
 LOAD DATA LOCAL
