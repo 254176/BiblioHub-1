fc6cf882c519d54818431f5628107c0a6e2326d3
Jaidyn Ann <10477760+JadedCtrl@users.noreply.github.com>
2024-04-17 08:50:46 -0500

    Remove superfluous logs

--- book_management.php
+++ book_management.php
@@ -91,67 +91,7 @@ require_once('lib/db.php');
 	}
 	?>
 </table>
-<h2>Transaction Log</h2>
-<table border="1">
-	<tr>
-		<th>TransactionID</th>
-		<th>UserID</th>
-		<th>InventoryID</th>
-		<th>TransactionType</th>
-		<th>TransactionDate</th>
-		<th>DueDate</th>
-		<th>Status</th>
-	</tr>
-	<?php
-	// Select data from Transactions table
-	$sql = "SELECT * FROM Transactions";
-	$result = $conn->query($sql);
-
-	// Output data of each row
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			echo "<tr>";
-			echo "<td>" . $row["TransactionID"] . "</td>";
-			echo "<td>" . $row["UserID"] . "</td>";
-			echo "<td>" . $row["InventoryID"] . "</td>";
-			echo "<td>" . $row["TransactionType"] . "</td>";
-			echo "<td>" . $row["TransactionDate"] . "</td>";
-			echo "<td>" . $row["DueDate"] . "</td>";
-			echo "<td>" . $row["Status"] . "</td>";
-			echo "</tr>";
-		}
-	} else {
-		echo "<tr><td colspan='7'>0 results</td></tr>";
-	}
-	?>
-</table>
-<h2>Inventory Log</h2>
-<table border="1">
-	<tr>
-		<th>InventoryID</th>
-		<th>ISBN</th>
-		<th>TransactionID</th>
-	</tr>
-	<?php
-	// Select data from Inventory table
-	$sql = "SELECT * FROM Inventory";
-	$result = $conn->query($sql);
-
-	// Output data of each row
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			echo "<tr>";
-			echo "<td>" . $row["InventoryID"] . "</td>";
-			echo "<td>" . $row["ISBN"] . "</td>";
-			echo "<td>" . $row["TransactionID"] . "</td>";
-			echo "</tr>";
-		}
-	} else {
-		echo "<tr><td colspan='3'>0 results</td></tr>";
-	}
-	$conn->close();
-	?>
-</table>
+<span><?php echo $result->num_rows ?> results</span>
 <script>
  // JavaScript validation for ISBN field in Add Book form
  document.getElementById('addBookForm').addEventListener('submit', function(event) {
--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,6 @@
 <?php
 $title = "Dashboard";
 include("lib/header.php");
-require_once('lib/db.php');
 ?>
 
 <h2>Book Transaction</h2>
@@ -24,94 +23,6 @@ require_once('lib/db.php');
 	<button type="submit">Submit Transaction</button>
 </form>
 
-<h2>Book Log</h2>
-<table border="1">
-	<tr>
-		<th>ISBN</th>
-		<th>Title</th>
-		<th>Author</th>
-		<th>Genre</th>
-	</tr>
-	<?php
-	// Select data from Books table
-	$sql = "SELECT * FROM Books";
-	$result = $conn->query($sql);
-
-	// Output data of each row
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			echo "<tr>";
-			echo "<td>" . $row["ISBN"] . "</td>";
-			echo "<td>" . $row["Title"] . "</td>";
-			echo "<td>" . $row["Author"] . "</td>";
-			echo "<td>" . $row["Genre"] . "</td>";
-			echo "</tr>";
-		}
-	} else {
-		echo "<tr><td colspan='4'>0 results</td></tr>";
-	}
-	?>
-</table>
-<h2>Inventory Log</h2>
-<table border="1">
-	<tr>
-		<th>InventoryID</th>
-		<th>ISBN</th>
-		<th>TransactionID</th>
-	</tr>
-	<?php
-	// Select data from Inventory table
-	$sql = "SELECT * FROM Inventory";
-	$result = $conn->query($sql);
-
-	// Output data of each row
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			echo "<tr>";
-			echo "<td>" . $row["InventoryID"] . "</td>";
-			echo "<td>" . $row["ISBN"] . "</td>";
-			echo "<td>" . $row["TransactionID"] . "</td>";
-			echo "</tr>";
-		}
-	} else {
-		echo "<tr><td colspan='3'>0 results</td></tr>";
-	}
-	?>
-</table>
-<h2>Transaction Log</h2>
-<table border="1">
-	<tr>
-		<th>TransactionID</th>
-		<th>UserID</th>
-		<th>InventoryID</th>
-		<th>TransactionType</th>
-		<th>TransactionDate</th>
-		<th>DueDate</th>
-		<th>Status</th>
-	</tr>
-	<?php
-	// Select data from Transactions table
-	$sql = "SELECT * FROM Transactions";
-	$result = $conn->query($sql);
-
-	// Output data of each row
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			echo "<tr>";
-			echo "<td>" . $row["TransactionID"] . "</td>";
-			echo "<td>" . $row["UserID"] . "</td>";
-			echo "<td>" . $row["InventoryID"] . "</td>";
-			echo "<td>" . $row["TransactionType"] . "</td>";
-			echo "<td>" . $row["TransactionDate"] . "</td>";
-			echo "<td>" . $row["DueDate"] . "</td>";
-			echo "<td>" . $row["Status"] . "</td>";
-			echo "</tr>";
-		}
-	} else {
-		echo "<tr><td colspan='7'>0 results</td></tr>";
-	}
-	$conn->close();
-	?>
-</table>
-</body>
-</html>
+<?php
+include("lib/footer.php");
+?>
